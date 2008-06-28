c906f23591973e93d5b77a02adb46d18e556de04
snarfle <snarfle@42635f36-7686-4ab2-bed1-1f49e16f4437>
2008-06-28 22:15:23 +0000

    Finished IWMBackupRestoreProps, IWMLicenseBackup, IWMLicenseRestore & IWMStatusCallback

---
+++ trunk/wmlib/test/v1.0/IWMLicenseRestoreTest.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+
+using WindowsMediaLib;
+
+namespace v1._0
+{
+    [ComVisible(true)]
+    public class IWMLicenseRestoreTest : IWMStatusCallback
+    {
+        const string wzBackupDir = @"c:\test";
+        private IWMLicenseRestore m_pIWMLicenseRestore;
+        private int m_LastResult;
+        private const int iAborted = (unchecked((int)0xc00d2768));
+
+        public void DoTests()
+        {
+            Config();
+
+            TestRestore();
+            TestRestore2();
+        }
+
+        private void TestRestore()
+        {
+            m_pIWMLicenseRestore.RestoreLicenses(BackupRestoreFlags.None, this);
+            System.Threading.Thread.Sleep(10000);
+            Debug.Assert(m_LastResult == 0);
+        }
+
+        private void TestRestore2()
+        {
+            m_pIWMLicenseRestore.RestoreLicenses(BackupRestoreFlags.None, this);
+            m_pIWMLicenseRestore.CancelLicenseRestore();
+            System.Threading.Thread.Sleep(10000);
+            Debug.Assert(m_LastResult == iAborted);
+        }
+
+        private void Config()
+        {
+            IWMBackupRestoreProps pIWMBackupRestoreProps;
+            IWMLicenseBackup pIWMLicenseBackup;
+
+            WMUtils.WMCreateBackupRestorer((IWMStatusCallback)this, out pIWMLicenseBackup);
+            m_pIWMLicenseRestore = (IWMLicenseRestore)pIWMLicenseBackup;
+
+            pIWMBackupRestoreProps = (IWMBackupRestoreProps)pIWMLicenseBackup;
+
+            byte[] b = Encoding.Unicode.GetBytes(wzBackupDir);
+            short iSizeWithNull = (short)(b.Length + 2);
+            IntPtr ip = Marshal.AllocCoTaskMem(iSizeWithNull);
+            Marshal.Copy(b, 0, ip, b.Length);
+            Marshal.WriteInt16(ip, b.Length, 0);
+
+            pIWMBackupRestoreProps.SetProp("RestorePath", AttrDataType.STRING, ip, iSizeWithNull);
+
+            Marshal.FreeCoTaskMem(ip);
+        }
+
+        #region IWMStatusCallback Members
+
+        void IWMStatusCallback.OnStatus(Status iStatus, int hr, AttrDataType dwType, IntPtr pValue, IntPtr pvContext)
+        {
+            Debug.Write(string.Format("{0} 0x{1:x} {2} {3} {4} {5} ", iStatus, hr, WMError.GetErrorText(hr), dwType, pValue.ToInt32(), pvContext.ToInt32()));
+            m_LastResult = hr;
+
+            switch (dwType)
+            {
+                case AttrDataType.STRING:
+                    Debug.WriteLine(Marshal.PtrToStringUni(pValue));
+                    break;
+                case AttrDataType.WORD:
+                    Debug.WriteLine(Marshal.ReadInt16(pValue));
+                    break;
+                case AttrDataType.DWORD:
+                case AttrDataType.BOOL:
+                    Debug.WriteLine(Marshal.ReadInt32(pValue));
+                    break;
+                case AttrDataType.QWORD:
+                    Debug.WriteLine(Marshal.ReadInt64(pValue));
+                    break;
+                default:
+                    Debug.WriteLine("");
+                    break;
+            }
+        }
+
+        #endregion
+    }
+}
