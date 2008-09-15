b882986093687cf13bf12073affadef358a3a619
bwkahle <bwkahle@42635f36-7686-4ab2-bed1-1f49e16f4437>
2008-09-15 01:12:31 +0000

    Updating todo and adding missing file.

---
+++ wmlib/test/v1.0/IWMReaderPlaylistBurnTest.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WindowsMediaLib;
+using System.Threading;
+using System.Diagnostics;
+
+namespace v1._0
+{
+    public class IWMReaderPlaylistBurnTest : IWMStatusCallback
+    {
+        IWMReaderPlaylistBurn burn;
+        private readonly object m_resultLock = new object();
+        private const int iAborted = (unchecked((int)0xc00d2768));
+        
+
+        public void DoTests()
+        {
+            Config();
+
+            TestInit();
+
+            //TestCancel();
+        }
+
+        private void TestInit()
+        {
+            string[] playlist = new string[1];
+            playlist[0] = @"C:\so_lesson3c.wmv";
+            burn.InitPlaylistBurn(1, playlist, this, new IntPtr(123));
+
+            lock (m_resultLock)
+            {
+                Monitor.Wait(m_resultLock);
+            }
+
+            int[] results = new int[1];
+            burn.GetInitResults(1, results);
+            Debug.Assert(results[0] == 0);
+
+            burn.EndPlaylistBurn(iAborted);
+        }
+
+        private void TestCancel()
+        {
+            string[] playlist = new string[1];
+            playlist[0] = @"C:\so_lesson3c.wmv";
+            burn.InitPlaylistBurn(1, playlist, this, new IntPtr(123));
+            burn.Cancel();
+        }
+
+        private void Config()
+        {
+            IWMReader reader;            
+            WMUtils.WMCreateReader(IntPtr.Zero, Rights.Playback, out reader);
+            burn = (IWMReaderPlaylistBurn)reader;
+        }
+
+        #region IWMStatusCallback Members
+
+        public void OnStatus(Status iStatus, int hr, AttrDataType dwType, IntPtr pValue, IntPtr pvContext)
+        {
+            if (iStatus == Status.InitPlaylistBurn)
+            {
+                lock (m_resultLock)
+                {
+                    Monitor.PulseAll(m_resultLock);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
