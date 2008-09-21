7bbfd503d8a60c9715622991daba7dd604693b76
snarfle <snarfle@42635f36-7686-4ab2-bed1-1f49e16f4437>
2008-09-21 01:43:45 +0000

    Adjust version number

--- wmlib/src/AssemblyInfo.cs
+++ wmlib/src/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Security.Permissions;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: Guid("FA40A71D-13EA-4890-B916-FD76A57D4BAE")]
-[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
 #if DEBUG
 [assembly: AssemblyProduct("Debug Version")]
 #else
